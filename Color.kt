8d0ecaf7d13f620c4bbae280b53307ffd65ae18c
Alex <134198480+LuckyWolf-svg@users.noreply.github.com>
2026-01-13 21:16:46 +0700

    Update Color.kt

--- Color.kt
+++ Color.kt
@@ -2,17 +2,14 @@ package com.example.myapplication.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// Нежно-голубые цвета
-val LightBlue = Color(0xFFE3F2FD)    // Светлый фон
-val MediumBlue = Color(0xFF90CAF9)   // Кнопки
-val DarkBlue = Color(0xFF1976D2)     // Акцентный цвет
+val LightBlue = Color(0xFFE3F2FD)
+val MediumBlue = Color(0xFF90CAF9)
+val DarkBlue = Color(0xFF1976D2)
 
-// Дополнительные цвета для UI
 val GrayBackground = Color(0xFFF5F5F5)
 val GrayBorder = Color(0xFFE0E0E0)
 val TextGray = Color(0xFF757575)
 
-// Оставьте старые цвета для совместимости, если нужно
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
